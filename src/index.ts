90925905faf517026733f2483239e6a06fc5c5a7
Ahmed-Abdella <abdellaa921@gmail.com>
2022-10-11 04:16:06 +0200

    creating the class user

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log("HI There !!!!");
+import axios from "axios";
+axios.post(" http://localhost:3000/users", {
+  name: "Ahmed",
+  age: 27,
+});
+
+axios.get("http://localhost:3000/users/1");
---
+++ src/models/User.ts
@@ -0,0 +1,34 @@
+interface Userprops {
+  name?: string;
+  age?: number;
+}
+
+type Callback = () => void;
+
+export class User {
+  events: { [key: string]: Callback[] } = {};
+
+  constructor(private data: Userprops) {}
+  get(propName: string): number | string {
+    return this.data[propName];
+  }
+
+  set(update: Userprops): void {
+    Object.assign(this.data, update);
+  }
+
+  on(eventName: string, callback: Callback): void {
+    const handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  }
+
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName];
+    if (!handlers || handlers.length === 0) {
+      return;
+    }
+
+    handlers.forEach((callBack) => callBack());
+  }
+}
